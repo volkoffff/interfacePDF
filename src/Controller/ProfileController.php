9d0bf29e755096c353bf570d02eda76156dc4435
volkoffff <robin.lanfranchi@etudiant.univ-reims.fr>
2024-05-21 12:19:59 +0200

    feat(profile): add profile page

---
+++ src/Controller/ProfileController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class ProfileController extends AbstractController
+{
+    #[Route('/profile', name: 'app_profile')]
+    public function index(): Response
+    {
+        $user = $this->getUser();
+        $subscription = $user->getSubscription();
+
+        return $this->render('profile/index.html.twig', [
+            'user' => $user,
+            'subscription' => $subscription,
+        ]);
+    }
+}
